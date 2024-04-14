44a032c773ced82b8154795c02abb7b6dc3917de
martinippo01 <martinippo@gmail.com>
2024-04-14 16:21:51 -0300

    Worked on notification servant

--- server/src/main/java/ar/edu/itba/pod/tpe1/data/Notifications.java
+++ server/src/main/java/ar/edu/itba/pod/tpe1/data/Notifications.java
@@ -8,6 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
 
 
 public class Notifications {
@@ -25,6 +26,38 @@ public class Notifications {
         return instance;
     }
 
+    public boolean registerAirline(Airline airline){
+        if(notifications.containsKey(airline))
+            return false;
+        notifications.put(airline, new LinkedBlockingQueue<>());
+        return true;
+    }
+
+    public boolean unregisterAirline(Airline airline){
+        if(!notifications.containsKey(airline))
+            return false;
+        notifications.remove(airline);
+        return true;
+    }
+
+    public void notifyAirline(Airline airline, Notification notification){
+        if(!notifications.containsKey(airline))
+            throw new IllegalArgumentException();
+        try {
+            notifications.get(airline).put(notification);
+        }catch (InterruptedException e){
+            // TODO check this try catch and which is the best way to solve this
+            e.printStackTrace();
+            System.err.println("Could not queue notification");
+        }
+    }
+
+    public Notification getNotification(Airline airline){
+        if(!notifications.containsKey(airline))
+            // This can happen if the airline got unregistered or it never was registered
+            return null;
+        return notifications.get(airline).poll();
+    }
 
 
 }
--- server/src/main/java/ar/edu/itba/pod/tpe1/servant/NotificationsServant.java
+++ server/src/main/java/ar/edu/itba/pod/tpe1/servant/NotificationsServant.java
@@ -4,6 +4,8 @@ import airport.NotificationsServiceGrpc;
 import airport.NotificationsServiceOuterClass;
 import ar.edu.itba.pod.tpe1.data.Airport;
 import ar.edu.itba.pod.tpe1.data.Notifications;
+import ar.edu.itba.pod.tpe1.data.utils.Airline;
+import ar.edu.itba.pod.tpe1.data.utils.Notification;
 import io.grpc.stub.StreamObserver;
 
 public class NotificationsServant extends NotificationsServiceGrpc.NotificationsServiceImplBase {
@@ -16,6 +18,20 @@ public class NotificationsServant extends NotificationsServiceGrpc.Notifications
             NotificationsServiceOuterClass.RegisterNotificationsRequest req,
             StreamObserver<NotificationsServiceOuterClass.RegisterNotificationsResponse> responseObserver
     ){
+        Airline airline = new Airline(req.getAirline());
+        // First register the airline
+        notifications.registerAirline(airline);
+        // TODO handle when it fails to register airline
+
+        Notification notification;
+        do{
+            notification = notifications.getNotification(airline);
+            if(notification != null){
+                responseObserver.onNext(NotificationsServiceOuterClass.RegisterNotificationsResponse.newBuilder().build());
+            }
+        }while (notification != null); // null is the poison pill
+
+        responseObserver.onCompleted();
 
     }
 
@@ -24,7 +40,15 @@ public class NotificationsServant extends NotificationsServiceGrpc.Notifications
             NotificationsServiceOuterClass.RemoveNotificationsRequest req,
             StreamObserver<NotificationsServiceOuterClass.RemoveNotificationsResponse> responseObserver
     ){
+        Airline airline = new Airline(req.getAirline());
 
+        boolean success = notifications.unregisterAirline(airline);
+        if(success){
+            responseObserver.onNext(NotificationsServiceOuterClass.RemoveNotificationsResponse.newBuilder().build());
+            responseObserver.onCompleted();
+        } else{
+            responseObserver.onError(io.grpc.Status.INVALID_ARGUMENT.withDescription("Airline is not registered").asRuntimeException());
+        }
     }
 
 }
