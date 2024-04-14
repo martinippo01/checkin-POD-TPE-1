e9ae2555a07d7545d506b9a2beebee3fceb85deb
martinippo01 <martinippo@gmail.com>
2024-04-14 12:39:19 -0300

    Working on servant

--- server/src/main/java/ar/edu/itba/pod/tpe1/servant/NotificationsServant.java
+++ server/src/main/java/ar/edu/itba/pod/tpe1/servant/NotificationsServant.java
@@ -1,4 +1,31 @@
 package ar.edu.itba.pod.tpe1.servant;
 
-public class NotificationsServant {
+import airport.NotificationsServiceGrpc;
+import airport.NotificationsServiceOuterClass;
+import ar.edu.itba.pod.tpe1.data.Airport;
+import ar.edu.itba.pod.tpe1.data.Notifications;
+import io.grpc.stub.StreamObserver;
+
+public class NotificationsServant extends NotificationsServiceGrpc.NotificationsServiceImplBase {
+
+    private final Airport airport = Airport.getInstance();
+    private final Notifications notifications = Notifications.getInstance();
+
+    @Override
+    public void registerNotifications(
+            NotificationsServiceOuterClass.RegisterNotificationsRequest req,
+            StreamObserver<NotificationsServiceOuterClass.RegisterNotificationsResponse> responseObserver
+    ){
+
+    }
+
+    @Override
+    public void removeNotifications(
+            NotificationsServiceOuterClass.RemoveNotificationsRequest req,
+            StreamObserver<NotificationsServiceOuterClass.RemoveNotificationsResponse> responseObserver
+    ){
+
+
+    }
+
 }
