1cb071c3ff85251e56514a92c212b9e3c5750c43
martinippo01 <martinippo@gmail.com>
2024-04-18 10:08:30 -0300

    Solve bug: Make the poisson pill so that consumer of Notifications wakes up

--- client/src/main/java/ar/edu/itba/pod/tpe1/client/Client.java
+++ client/src/main/java/ar/edu/itba/pod/tpe1/client/Client.java
@@ -10,61 +10,20 @@ import java.util.concurrent.TimeUnit;
 public class Client {
     private static Logger logger = LoggerFactory.getLogger(Client.class);
 
-//    public static void main(String[] args) throws InterruptedException {
-//        logger.info("grpc-com-tpe1 Client Starting ...");
-//        logger.info("grpc-com-patterns Client Starting ...");
-//        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50058)
-//                .usePlaintext()
-//                .build();
-//        try {
-//            //AirportAdminClient airportAdminClient = new AirportAdminClient(channel);
-//            NotificationsClient notificationsClient = new NotificationsClient(channel);
-//            notificationsClient.registerNotifications("AmericanAirlines");
-//            notificationsClient.unregisterNotifications("AmericanAirlines");
-//        } finally {
-//            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
-//        }
-//    }
-
-
-        public static void main(String[] args) {
-
-            logger.info("grpc-com-tpe1 Client Starting ...");
-            logger.info("grpc-com-patterns Client Starting ...");
-            ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50058)
+    public static void main(String[] args) throws InterruptedException {
+        logger.info("grpc-com-tpe1 Client Starting ...");
+        logger.info("grpc-com-patterns Client Starting ...");
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50058)
                 .usePlaintext()
                 .build();
+        try {
+            //AirportAdminClient airportAdminClient = new AirportAdminClient(channel);
             NotificationsClient notificationsClient = new NotificationsClient(channel);
-            // Thread 1
-            Thread thread1 = new Thread(() -> {
-                System.out.println("Thread 1 started");
-                try {
-                    Thread.sleep(100); // Sleep
-
-                    notificationsClient.registerNotifications("AmericanAirlines");
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread 1 completed");
-            });
-
-            // Thread 2
-            Thread thread2 = new Thread(() -> {
-                System.out.println("Thread 2 started");
-                try {
-                    Thread.sleep(5000); // Sleep for 5 seconds
-                    notificationsClient.unregisterNotifications("AmericanAirlines");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread 2 completed");
-            });
-
-            // Start both threads
-            thread1.start();
-            thread2.start();
+            notificationsClient.registerNotifications("AmericanAirlines");
+            notificationsClient.unregisterNotifications("AmericanAirlines");
+        } finally {
+            channel.shutdown().awaitTermination(10, TimeUnit.SECONDS);
         }
-
+    }
 
 }
