ce0f6bae17a7c9b545287d948d89f11bd0ce06b3
Martín E. Zahnd <mzahnd@itba.edu.ar>
2024-04-27 19:36:49 -0300

    Add support for run_server script

--- server/src/main/assembly/overlay/run-server.sh
+++ server/src/main/assembly/overlay/run-server.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+#
+#
+# Ignore SC2155: Declare and assign separately to avoid masking return values.
+# shellcheck disable=SC2155
+#
 
-PATH_TO_CODE_BASE=`pwd`
+# Enter script directory before sourcing anything
 
-#JAVA_OPTS="-Djava.security.debug=access -Djava.security.manager -Djava.security.policy=/$PATH_TO_CODE_BASE/java.policy -Djava.rmi.server.useCodebaseOnly=false"	
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+if ! pushd "${SCRIPT_DIR}" &> /dev/null; then
+        >&2 echo "Script directory not found (??): '$SCRIPT_DIR'"
+        exit 1
+fi
 
+PATH_TO_CODE_BASE="$(pwd)"
+JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/grpc-com-tpe1-server-2024.1Q.jar"
 MAIN_CLASS="ar.edu.itba.pod.tpe1.server.Server"
 
+java "$JAVA_OPTS" -cp 'lib/jars/*' "$MAIN_CLASS" "$@"
 
-java  $JAVA_OPTS -cp 'lib/jars/*' $MAIN_CLASS $*
+# Exit script directory
+popd &> /dev/null || exit 0
