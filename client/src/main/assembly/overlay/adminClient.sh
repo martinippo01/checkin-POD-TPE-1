ef2e4a24535762d538f85e96c93e8bee37dcf3d1
Martín E. Zahnd <mzahnd@itba.edu.ar>
2024-04-27 16:20:34 -0300

    Update adminClient assembly
    * Use Bash good practices

--- client/src/main/assembly/overlay/adminClient.sh
+++ client/src/main/assembly/overlay/adminClient.sh
@@ -1,11 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+#
+# Ignore SC2155: Declare and assign separately to avoid masking return values.
+# shellcheck disable=SC2155
+#
 
-PATH_TO_CODE_BASE=`pwd`
+# Enter script directory before sourcing anything
 
-JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/grpc-com-tpe1-client-2024.1Q.jar"
-#JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/rmi-params-client-1.0-SNAPSHOT.jar"
+readonly SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+if ! pushd "${SCRIPT_DIR}" &> /dev/null; then
+        >&2 echo "Script directory not found (??): '$SCRIPT_DIR'"
+        exit 1
+fi
 
-MAIN_CLASS="ar.edu.itba.pod.tpe1.client.AirportAdminClient"
-#MAIN_CLASS="ar.edu.itba.pod.tpe1.client.Client
+readonly PATH_TO_CODE_BASE="$(pwd)"
+readonly JAVA_OPTS="-Djava.rmi.server.codebase=file://$PATH_TO_CODE_BASE/lib/jars/grpc-com-tpe1-client-2024.1Q.jar"
+readonly MAIN_CLASS="ar.edu.itba.pod.tpe1.client.admin.AirportAdminClient"
 
-java $JAVA_OPTS -cp 'lib/jars/*'  $MAIN_CLASS $#
+java "$JAVA_OPTS" -cp 'lib/jars/*' "$MAIN_CLASS" "$@"
+
+# Exit script directory
+popd &> /dev/null || exit 0
