ff182fc3650407e90d2a0076995660672f2806e9
Martín E. Zahnd <mzahnd@itba.edu.ar>
2024-04-27 11:10:14 -0300

    Add support for optional arguments

--- client/src/main/java/ar/edu/itba/pod/tpe1/client/Action.java
+++ client/src/main/java/ar/edu/itba/pod/tpe1/client/Action.java
@@ -6,21 +6,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.naming.ServiceUnavailableException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public abstract class Action {
     private static final Logger logger = LoggerFactory.getLogger(Action.class);
 
     private final List<String> actionArguments;
+    private final List<String> optionalActionArguments;
     private final HashMap<String, String> arguments = new HashMap<>(1);
 
 
     public Action(final List<String> actionArguments) {
         Objects.requireNonNull(actionArguments, "Received NULL 'actionArguments'");
         this.actionArguments = actionArguments;
+        this.optionalActionArguments = Collections.emptyList();
+    }
+
+    public Action(final List<String> actionArguments, final List<String> optionalActionArguments) {
+        Objects.requireNonNull(actionArguments, "Received NULL 'actionArguments'");
+        Objects.requireNonNull(optionalActionArguments, "Received NULL 'optionalActionArguments'");
+        this.actionArguments = actionArguments;
+        this.optionalActionArguments = optionalActionArguments;
     }
 
     public abstract void run(final ManagedChannel channel) throws ServerUnavailableException;
@@ -38,11 +44,17 @@ public abstract class Action {
 
         for (String actionArgument : actionArguments) {
             if (!arguments.containsKey(actionArgument)) {
-                logger.error("Expected {} argument, but found nothing.", actionArgument);
+                logger.error("Expected required {} argument, but found nothing.", actionArgument);
                 throw new IllegalArgumentException(Util.EXCEPTION_MESSAGE_INVALID_ARGUMENT + actionArgument);
             }
 
             this.arguments.put(actionArgument, arguments.get(actionArgument));
+        }
+
+        for (String actionArgument : optionalActionArguments) {
+            if (arguments.containsKey(actionArgument)) {
+                this.arguments.put(actionArgument, arguments.get(actionArgument));
+            }
         }
     }
 }
