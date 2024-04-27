741e5f54453b8077cf15840aa8d16ef43c7f21e6
Marco4444 <mscilipoti@itba.edu.ar>
2024-04-27 17:42:28 -0300

    added checking write to file

--- client/src/main/java/ar/edu/itba/pod/tpe1/client/query/actions/CheckIns.java
+++ client/src/main/java/ar/edu/itba/pod/tpe1/client/query/actions/CheckIns.java
@@ -6,34 +6,39 @@ import ar.edu.itba.pod.tpe1.client.exceptions.ServerUnavailableException;
 import ar.edu.itba.pod.tpe1.client.query.CounterQueryAction;
 import io.grpc.ManagedChannel;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
-import static ar.edu.itba.pod.tpe1.client.Arguments.AIRLINE;
-import static ar.edu.itba.pod.tpe1.client.Arguments.SECTOR;
+import static ar.edu.itba.pod.tpe1.client.Arguments.*;
 
 public final class CheckIns extends CounterQueryAction {
-    private CounterServiceGrpc.CounterServiceBlockingStub blockingStub;
 
     public CheckIns(List<String> actionArguments, List<String> optionalActionArguments) {
         super(actionArguments, optionalActionArguments);
     }
 
-    private void printCheckInsQueryResponse(CounterServiceOuterClass.QueryCheckInsResponse response) {
-        System.out.println("   Sector  Counter   Airline           Flight     Booking");
-        System.out.println("   ###############################################################");
-        for (CounterServiceOuterClass.CheckInRecord record : response.getCheckInsList()) {
-            System.out.printf("   %-7s %-9d %-17s %-9s %-6s%n",
-                    record.getSector(),
-                    record.getCounter(),
-                    record.getAirline(),
-                    record.getFlight(),
-                    record.getBookingCode());
+    private void printCheckInsQueryResponse(CounterServiceOuterClass.QueryCheckInsResponse response, String outPath) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outPath))) {
+            writer.write("   Sector  Counter   Airline           Flight     Booking\n");
+            writer.write("   ###############################################################\n");
+            for (CounterServiceOuterClass.CheckInRecord record : response.getCheckInsList()) {
+                writer.write(String.format("   %-7s %-9d %-17s %-9s %-6s%n",
+                        record.getSector(),
+                        record.getCounter(),
+                        record.getAirline(),
+                        record.getFlight(),
+                        record.getBookingCode()));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void run(ManagedChannel channel) throws ServerUnavailableException {
-        blockingStub = CounterServiceGrpc.newBlockingStub(channel);
+        CounterServiceGrpc.CounterServiceBlockingStub blockingStub = CounterServiceGrpc.newBlockingStub(channel);
 
         CounterServiceOuterClass.QueryCheckInsRequest request = CounterServiceOuterClass.QueryCheckInsRequest.newBuilder()
                 .setSector(getArguments().get(SECTOR.getArgument()))
@@ -44,6 +49,6 @@ public final class CheckIns extends CounterQueryAction {
             System.out.println("No check-ins found for the specified criteria.");
             return;
         }
-        printCheckInsQueryResponse(response);
+        printCheckInsQueryResponse(response, getArguments().get(OUT_PATH.getArgument()));
     }
 }
