39d8b2f81c3803b5b5c548c3131285d1485f9660
Marco4444 <mscilipoti@itba.edu.ar>
2024-04-28 12:11:17 -0300

    added client printing to passenger checkin

--- client/src/main/java/ar/edu/itba/pod/tpe1/client/checkin/actions/PassengerCheckin.java
+++ client/src/main/java/ar/edu/itba/pod/tpe1/client/checkin/actions/PassengerCheckin.java
@@ -1,8 +1,6 @@
 package ar.edu.itba.pod.tpe1.client.checkin.actions;
 
-import ar.edu.itba.pod.tpe1.CheckinServiceGrpc;
-import ar.edu.itba.pod.tpe1.PassengerCheckinRequest;
-import ar.edu.itba.pod.tpe1.PassengerCheckinResponse;
+import ar.edu.itba.pod.tpe1.*;
 import ar.edu.itba.pod.tpe1.client.checkin.CheckInAction;
 import ar.edu.itba.pod.tpe1.client.exceptions.ServerUnavailableException;
 import io.grpc.ManagedChannel;
@@ -39,6 +37,21 @@ public final class PassengerCheckin extends CheckInAction {
         try {
             PassengerCheckinRequest request = createRequest();
             PassengerCheckinResponse response = fetchResponse(request);
+
+            if (response.getStatus() == CheckinStatus.CHECKIN_STATUS_ADDED_TO_QUEUE) {
+                BookingInformation bookingInfo = response.getBooking();
+                CountersInformation countersInfo = response.getData();  // Assuming the response has the counters info as optional
+                CounterRange range = countersInfo.getCounters();
+                int firstCounter = range.getFirstCounterNumber();
+                int lastCounter = firstCounter + range.getNumberOfConsecutiveCounters() - 1;
+                String sector = countersInfo.getSectorName();
+                int queueLength = countersInfo.getPeopleInQueue();
+
+                System.out.printf("Booking %s for flight %s from %s is now waiting to check-in on counters (%d-%d) in Sector %s with %d people in line%n",
+                        bookingInfo.getBookingCode(), bookingInfo.getFlightCode(), bookingInfo.getAirlineName(), firstCounter, lastCounter, sector, queueLength);
+            } else {
+                handleCheckinErrors(response.getStatus());
+            }
         } catch (StatusRuntimeException e) {
             if (e.getStatus().equals(Status.INVALID_ARGUMENT)) {
                 throw new IllegalArgumentException(e);
@@ -47,4 +60,16 @@ public final class PassengerCheckin extends CheckInAction {
             }
         }
     }
+
+    private void handleCheckinErrors(CheckinStatus status) {
+        switch (status) {
+            case CHECKIN_STATUS_INVALID_BOOKING_CODE -> System.out.println("Error: Invalid booking code.");
+            case CHECKIN_STATUS_INVALID_SECTOR_ID -> System.out.println("Error: Invalid sector ID.");
+            case CHECKIN_STATUS_INVALID_COUNTER_NUMBER -> System.out.println("Error: Invalid counter number.");
+            case CHECKIN_STATUS_PASSENGER_ALREADY_IN_QUEUE -> System.out.println("Error: Passenger already in queue.");
+            case CHECKIN_STATUS_CHECKIN_ALREADY_DONE -> System.out.println("Error: Check-in already completed for this passenger.");
+            default -> System.out.println("Error: An unknown error occurred.");
+        }
+    }
+
 }
